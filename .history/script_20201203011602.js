71d252d7455694c4e862c4012e2bc4c879437060
MAC <mac@MACs-MacBook-Pro.local>
2020-12-03 01:31:47 +0100

    working on the functionality of the ticketApp with js Dom

--- .history/script_20201203011602.js
+++ .history/script_20201203011602.js
@@ -2,5 +2,3 @@ const container = document.querySelector(".container");
 
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
-const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie");
--- .history/script_20201203013025.js
+++ .history/script_20201203013025.js
@@ -3,4 +3,4 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie");
+const movieSelect = doc
